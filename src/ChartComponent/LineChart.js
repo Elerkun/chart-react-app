3a4a18c9eacbf9662039e53be496e0756aee198f
Elerkun <thejoncelito07.jg@gmail.com>
2022-04-15 19:21:25 +0200

    FEAT: xAxes format time

--- src/ChartComponent/LineChart.js
+++ src/ChartComponent/LineChart.js
@@ -2,6 +2,7 @@ import { useMemo } from "react";
 import {
   Chart as ChartJS,
   CategoryScale,
+  TimeScale,
   LinearScale,
   PointElement,
   LineElement,
@@ -10,9 +11,11 @@ import {
   Legend,
   Filler,
 } from "chart.js";
-import { Line, Lin } from "react-chartjs-2";
+import { Line } from "react-chartjs-2";
+import 'chartjs-adapter-moment';
 
 ChartJS.register(
+  TimeScale,
   CategoryScale,
   LinearScale,
   PointElement,
@@ -25,7 +28,27 @@ ChartJS.register(
 
 const power = [6, 7, 10, 4, 5, 8, 9];
 const power2 = [1, 3, 2, 2, 4, 4, 5, 3, 2];
-const labels = ["10:00", "10:05", "10:10", "10:15", "10:20", "10:25", "10:30"];
+//const labels = ["10:00", "10:05", "10:10", "10:15", "10:20", "10:25", "10:30"];
+const meterData = [{
+  x: '2021-11-06 23:39:30',
+  y: 10
+}, {
+  x: '2021-11-06 01:00:28',
+  y: 9
+}, {
+  x: '2021-11-07 09:00:28',
+  y: 8
+}];
+const powerSet = [{
+  x: '2021-11-06 23:40:35',
+  y: 3
+}, {
+  x: '2021-11-06 01:00:28',
+  y: 6
+}, {
+  x: '2021-11-06 09:00:28',
+  y: 3
+}];
 const options = {
       responsive: true,
       interaction: {
@@ -46,39 +69,54 @@ const options = {
           display: true,
           position: 'left',
         },
-        y1: {
-            beginAtZero: true,
-          type: 'linear',
-          display: true,
-          position: 'right',
-        },
-      }
+        // y1: {
+        //     beginAtZero: true,
+        //   type: 'linear',
+        //   display: true,
+        //   position: 'right',
+        // },
+        x: {
+          type: "time",
+          time: {
+            unit: 'minute',
+            // format: 'HH:mm',
+            stepSize: 15,
+            //unitStepSize: 15,
+            displayFormats: {
+              'minute': 'DD MMM YYY HH:mm',
+              'hour': 'HH',
+              'day': 'DD'
+            },
+            // unit: "day"
+          }
+        }
+    }
 };
 export const LineChart = () => {
   const data = useMemo(function () {
     return {
       datasets: [
         {
-          label: "Mis datos",
-          data: power,
+          label: "Meter Data",
+          data: meterData,
           tension: 0.3,
           borderColor: "rgb(75, 192, 192)",
           pointRadius: 6,
           pointBackgroundColor: "rgb(75, 192, 192)",
           backgroundColor: "rgba(75, 192, 192, 0.3)",
-          yAxisID: 'y',
+         //yAxisID: 'y',
         },
         {
-          label: "Mis datos (2)",
+          label: "Power Set",
           tension: 0.3,
-          data: power2,
+          data: powerSet,
           borderColor: "green",
           backgroundColor: "rgba(0, 255, 0, 0.3)",
           pointRadius: 6,
-          yAxisID: 'y1',
+         //yAxisID: 'y1',
         },
       ],
-      labels,
+      //labels,
     };
   }, []);
   return <Line data={data} options={options} />;
